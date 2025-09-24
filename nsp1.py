b0e97e775bd582ea4b6888ec5af4458e88389e96
Sidney Junior <128847258+sidneyjunr@users.noreply.github.com>
2025-09-24 14:47:28 -0300

    Ajustes

--- nsp1.py
+++ nsp1.py
@@ -1,12 +1,13 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
 import os
 import cv2 
 import umap
+import matplotlib.pyplot as plt
 
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
-
+from sklearn.cluster import KMeans
+from sklearn_extra.cluster import KMedoids
 
 lista_pastas = os.listdir("RecFac")
 X = np.empty((128*120,0))
@@ -21,7 +22,7 @@ for pasta in lista_pastas:
                 ))
 
 
-print(X.shape)
+# print(X.shape)
 X = X.T
 
 #1: tsne
@@ -30,9 +31,6 @@ X = X.T
 
 #X_red1 = tsne.fit_transform(X)
 #print(X_red1.shape)
-#plt.figure(1)
-#plt.scatter(X_red1[:,0],X_red1[:,1], edgecolors='k',c='green')
-#plt.show()
 
 #2: PCA
 pca = PCA()
@@ -40,12 +38,8 @@ pca = PCA()
 
 pca.fit(X)
 
-#plt.figure(2)
 #x2 = np.arange(1,641,1)
 #y2 = np.cumsum(pca.explained_variance_ratio_)
-#plt.plot(x2,y2,marker='x')
-#plt.grid()
-#plt.show()
 
 
 #variância de 90%: x = 71.77 = 72
@@ -53,12 +47,8 @@ pca.fit(X)
 #pca = PCA(n_components=72)
 #X_red2 = pca.fit_transform(X)
 
-#plt.figure(2)
 #x2 = np.arange(1,73,1)
 #y2 = np.cumsum(pca.explained_variance_ratio_)
-#plt.plot(x2,y2,marker='x')
-#plt.grid()
-#plt.show()
 
 
 #variância de 80%: x = 29.62 = 30
@@ -66,12 +56,8 @@ pca.fit(X)
 #pca = PCA(n_components=30)
 #X_red3 = pca.fit_transform(X)
 
-#plt.figure(2)
 #x2 = np.arange(1,31,1)
 #y2 = np.cumsum(pca.explained_variance_ratio_)
-#plt.plot(x2,y2,marker='x')
-#plt.grid()
-#plt.show()
 
 
 #variância de 75%: x = 21.68 = 22
@@ -80,21 +66,14 @@ pca.fit(X)
 pca = PCA(n_components=22)
 X_red4 = pca.fit_transform(X)
 
-plt.figure(2)
-x2 = np.arange(1,23,1)
-y2 = np.cumsum(pca.explained_variance_ratio_)
-plt.plot(x2,y2,marker='x')
-plt.grid()
-plt.show()
+# x2 = np.arange(1,23,1)
+# y2 = np.cumsum(pca.explained_variance_ratio_)
 
 
 #3: UMAP
 #umap_var = umap.UMAP(n_components=2)
 #X_red5 = umap_var.fit_transform(X)
 
-#plt.figure(3)
-#plt.scatter(X_red5[:,0],X_red5[:,1], c='r', edgecolor='k')
-#plt.show()
 
 
 
@@ -102,35 +81,51 @@ plt.show()
 #umap_var = umap.UMAP(n_components=3)
 #X_red6 = umap_var.fit_transform(X)
 
-#plt.figure(4)
-#plt.scatter(X_red6[:,0],X_red6[:,1], c='y', edgecolor='k')
-#plt.show()
 
 #dimensões: 15
 #umap_var = umap.UMAP(n_components=15)
 #X_red7 = umap_var.fit_transform(X)
 
-#plt.figure(5)
-#plt.scatter(X_red7[:,0],X_red7[:,1], c='m', edgecolor='k')
-#plt.show()
 
 
 #dimensões: 55
 #umap_var = umap.UMAP(n_components=55)
 #X_red8 = umap_var.fit_transform(X)
 
-#plt.figure(6)
-#plt.scatter(X_red8[:,0],X_red8[:,1], c='c', edgecolor='k')
-#plt.show()
 
 
 #dimensões: 101
 #umap_var = umap.UMAP(n_components=101)
 #X_red9 = umap_var.fit_transform(X)
 
-#plt.figure(7)
-#plt.scatter(X_red9[:,0],X_red9[:,1], c='b', edgecolor='k')
-#plt.show()
+
+
+
+
+
+# # Supondo que X_red2 seja seu PCA com n_components=72
+# X_red2 = pca.fit_transform(X)  # se ainda não tiver rodado
+
+# for k in [2, 3, 4, 5]:
+#     kmeans = KMeans(n_clusters=k, random_state=42)
+#     labels = kmeans.fit_predict(X_red2)
+
+#     # Visualização: se tiver mais de 2 dimensões, pega só as duas primeiras para o gráfico
+#     plt.scatter(X_red2[:, 0], X_red2[:, 1], c=labels, cmap='tab10')
+#     plt.title(f"K-Means com k={k}")
+#     plt.show()
+
+
+
+for k in [2, 3, 4, 5]:
+    kmedoids = KMedoids(n_clusters=k, random_state=42)
+    labels = kmedoids.fit_predict(X_red2)
+
+    plt.scatter(X_red2[:, 0], X_red2[:, 1], c=labels, cmap='tab10')
+    plt.title(f"K-Medoids com k={k}")
+    plt.show()
+
+
 
 
 bp = 1
